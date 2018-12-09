2885263ce6b7686f499483380d579ad3031248cf
Grant Gumina <grantgumina@gmail.com>
2018-12-08 20:56:36 -0800

    gonna use node/mongo setup

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// var UserController = require('./user/UserController');
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  // .use('/users', UserController);
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
